0f688c13666c06524c44db53eea46690f1850177
shubham-1809 <shubhamchouksey1809@gmail.com>
2023-11-08 20:07:22 +0530

    added share feature and script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('copy-link-button').addEventListener('click', function() {
+    // Get the URL of your portfolio website
+    const websiteURL = 'https://shubham-1809.github.io/My-Personal-Portfolio/';
+
+    // Create a temporary input element
+    const tempInput = document.createElement('input');
+    tempInput.value = websiteURL;
+
+    // Append the input element to the document
+    document.body.appendChild(tempInput);
+
+    // Select the input content
+    tempInput.select();
+
+    // Copy the selected text to the clipboard
+    document.execCommand('copy');
+
+    // Remove the temporary input element
+    document.body.removeChild(tempInput);
+
+    // Provide user feedback (e.g., tooltip or message)
+    alert('Link copied to clipboard');
+  });
+
+  document.getElementById('clicked').addEventListener('click', function(){
+    alert('Hey man! You are viewing it live...hahaha');
+  })
